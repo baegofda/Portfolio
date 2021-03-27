1df952c3dbece116ef450829e3f271991ef7301d
baegofda <molly.daleo3o@gmail.com>
2021-03-28 04:44:30 +0900

    Add menu js

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,5 +1,5 @@
 const gnb = document.querySelector(".gnb");
-
+const navItems = document.querySelectorAll(".header__container--items__item");
 gnb.addEventListener("click", () => {
   const menuBtn = document.querySelector(".gnb-menu");
   const closeBtn = document.querySelector(".gnb-close");
@@ -7,4 +7,14 @@ gnb.addEventListener("click", () => {
   menuBtn.classList.toggle("hide");
   closeBtn.classList.toggle("hide");
   menuList.classList.toggle("active");
+});
+
+const itemHandler = (item) => {
+  const itemTarget = item.currentTarget;
+  navItems.forEach((item) => item.classList.remove("active"));
+  itemTarget.classList.add("active");
+};
+
+navItems.forEach((item) => {
+  item.addEventListener("click", itemHandler);
 });
